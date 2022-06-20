d3ae839482f67b5420b3490f8871623445cd1360
linn-khant <linkhantnyinyi242@gmail.com>
2022-06-20 15:29:43 -0700

    refactor nav

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,63 @@
+  <!---------------------------NavBar---------------------------------->
+    <div class="container-xxl bg shadow">
+      <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand text-white" href="#"
+          >DearRosie <img src="./img/buterflies.png" height="40px" width="40px"
+        /></a>
+
+        <button
+          class="navbar-toggler bg-light"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="link">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a
+                  class="nav-link active mt-2 text-white"
+                  aria-current="page"
+                  href="posts.html"
+                  >Posts</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active mt-2 text-white" href="register.html"
+                  >Register</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active mt-2 text-white" href="login.html"
+                  >Login</a
+                >
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link mt-1 active text-white"
+                  data-bs-toggle="offcanvas"
+                  href="#offcanvasExample"
+                  role="button"
+                  aria-controls="offcanvasExample"
+                >
+                  Name<img
+                    src="./img/blank-profile-picture-973460__340.webp"
+                    class="rounded-circle ms-3 mb-1"
+                    height="40px"
+                    width="40px"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+    
+    <x-offcanvas/>
+    
---
+++ resources/views/components/offcanvas.blade.php
@@ -0,0 +1,50 @@
+<!--offcanvas------------>
+<div
+      class="offcanvas offcanvas-start"
+      tabindex="-1"
+      id="offcanvasExample"
+      aria-labelledby="offcanvasExampleLabel"
+    >
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title font fs-1" id="offcanvasExampleLabel">
+          Name
+        </h5>
+
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+
+      <div class="offcanvas-body">
+        <div class="border-top border-bottom border-secondary p-3">
+          <button
+            class="history btn border-0 text-start font"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#multiCollapseExample2"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample2"
+          >
+            History
+          </button>
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="collapse multi-collapse" id="multiCollapseExample2">
+              <div class="mt-1 p-2 border-bottom border-secondary font">
+                Purchase item name
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="border-bottom border-secondary p-3">
+          <button class="history btn border-0 text-start font" type="button">
+            Log out
+          </button>
+        </div>
+      </div>
+    </div>
