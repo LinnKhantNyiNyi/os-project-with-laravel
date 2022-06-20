ab0afde7524b8a61ccf9933020f8cb6127c2cc2f
linn-khant <linkhantnyinyi242@gmail.com>
2022-06-20 16:34:46 -0700

    add banner

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,30 @@
+  <!--Banner-------------->
+
+    <section class="container-xxl banner">
+      <div class="d-flex justify-content-center mobile">
+        <h1 class="font text-dark logo">DearRosie</h1>
+      </div>
+
+      <div class="d-flex justify-content-around">
+        <div class="d-flex align-items-center ms-2">
+          <h1 class="display-1 mainlogo desktop">DearRosie</h1>
+        </div>
+
+        <div class="d-flex justify-content-center me-4">
+          <div class="d-flex align-items-center bimage1">
+            <div class="p-2 my-4 bg-white shadow-lg desktop">
+              <img src="img/dr1.jpg" class=" " height="200px" width="200px" />
+            </div>
+          </div>
+
+          <div class="d-flex flex-column mb-1">
+            <div class="p-2 my-4 bg-white shadow-lg desktop">
+              <img src="img/dr2.jpg" class=" " height="200px" width="200px" />
+            </div>
+            <div class="p-2 my-4 bg-white shadow-lg desktop">
+              <img src="img/dr3.jpg" class=" " height="200px" width="200px" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
     <x-nav/>
+    <x-banner/>
 </x-layout>
